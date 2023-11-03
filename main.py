76c0f0c9f78ce278ebc7f925e0ba488862e065f8
York MacBook 018 <yorkmacbook018@Yorks-MacBook-Pro.local>
2023-11-03 16:19:31 -0500

    everything

---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    root_directory = '/Users/yorkmacbook018/PycharmProjects/pythonProject1/data/Albums'
+    output_csv = 'taylorLyrics.csv'
+    create_csv_from_txt(root_directory, output_csv)
